78f59fccef2d416d160aa95711a10ecabe127ab6
girikgarg8 <girikgarg8@gmail.com>
2023-02-21 00:03:46 +0530

    somewhere near around 1 hour

--- my-youtube/src/App.js
+++ my-youtube/src/App.js
@@ -1,10 +1,23 @@
-
+import './App.css';
+import Head from './components/Head';
+import Body from './components/Body';
 function App() {
   return (
     <div className="App">
-        <h1> Namaste React </h1>
+        <Head/>
+        <Body/>
     </div>
   );
 }
 
+/*
+Head
+Body 
+Sidebar
+MenuItems
+MainContainer
+ButtonsList
+VideoContainer
+VideosCard
+*/
 export default App;
