c3ef7cbe8b373a94b6cea2c7a1e50455f329c860
girikgarg8 <girikgarg8@gmail.com>
2023-02-27 23:16:02 +0530

    37:15

--- my-youtube/src/components/VideoContainer.js
+++ my-youtube/src/components/VideoContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../utils/constants'
-import VideoCard from './VideoCard'
+import VideoCard, { AdVideoCard } from './VideoCard'
 import {Link} from "react-router-dom"
 const VideoContainer = () => {
   const [videos, setVideos] = useState([])
@@ -14,7 +14,9 @@ const VideoContainer = () => {
     getVideos();
   }, []);
   return (
-    <div className='flex flex-wrap'> {
+    <div className='flex flex-wrap'>
+      {/* {videos[0] && <AdVideoCard info={videos[0]} />} */}
+      {
       videos.map((video) => (<Link to={"/watch?v=" + video.id.videoId} key={video.id.videoId}>
       < VideoCard key={video.id.videoId} info={video} />
       </Link>
